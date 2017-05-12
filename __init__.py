eebb12b06c60b136d0409e35e1810ebeadc5b943
Hugo van den Berg <hugo.van.den.berg@brabantwater.nl>
2017-05-12 16:14:07 +0200

    Functies hernoemd overeenkomstig PI DataLink

--- __init__.py
+++ __init__.py
@@ -1,5 +1,7 @@
 import sys
 import datetime
+import pytz
+import atexit
 
 import clr
 import pandas as pd
@@ -22,30 +24,77 @@ from OSIsoft.AF import Time
 from OSIsoft.AF import UI
 from OSIsoft.AF import UnitsOfMeasure
 
+piServer = None
+piafServer = None
+piafDB = None
 
-def connect_to_PIserver(serverName):
-    piServers = PI.PIServers()
+def connectPIServer(serverName = ''):
     global piServer
-    piServer = piServers[serverName]
+    piServers = PI.PIServers()
+    if serverName == '':
+        piServer = piServers.DefaultPIServer
+    else:
+        piServer = piServers[serverName]
     piServer.Connect(False)
 
-def connect_to_PIAFserver(serverName):
-    piServers = AF.PISystems()
+def connectPIAFServer(serverName = ''):
     global piafServer
     global piafDB
-    piafServer = piServers[serverName]
+    piServers = AF.PISystems()
+    if serverName == '':
+        piafServer = piServers.DefaultPISystem
+    else:
+        piafServer = piServers[serverName]
     piafServer.Connect()
     piafDB = piafServer.Databases.DefaultDatabase
 
-def get_tag_snapshot(tagname):
+def CurrentValue(tagname):
+    global piServer
+    if not piServer:
+        connectPIServer()
     tag = PI.PIPoint.FindPIPoint(piServer, tagname)
     lastData = tag.Snapshot()
-    return lastData.Value, lastData.Timestamp
+    return pd.DataFrame([__value_to_dict(lastData.Value, lastData.Timestamp.UtcTime)])
 
-def get_tag_values(tagname, starttime, endtime):
+def CompressedData(tagname, starttime, endtime):
+    global piServer
+    if not piServer:
+        connectPIServer()
     timeRange = Time.AFTimeRange(starttime, endtime)
     tag = PI.PIPoint.FindPIPoint(piServer, tagname)
-    pivalues = tag.RecordedValues(timeRange, 0, None, None)
-    df = pd.DataFrame([{'Value': x.Value, 'Timestamp': x.Timestamp.LocalTime} for x in pivalues])
-    df.Timestamp = df.Timestamp.apply(lambda x: datetime.datetime(x.Year, x.Month, x.Day, x.Hour, x.Minute, x.Second, x.Millisecond*1000), convert_dtype = True)
-    return df
+    pivalues = tag.RecordedValues(timeRange, Data.AFBoundaryType.Inside, None, None)
+    return pd.DataFrame([__value_to_dict(x.Value, x.Timestamp.UtcTime) for x in pivalues])
+
+def SampledData(tagname, starttime, endtime, interval):
+    global piServer
+    if not piServer:
+        connectPIServer()
+    timeRange = AFTimeRange(starttime, endtime)
+    span = AFTimeSpan.Parse(interval)
+    tag = PIPoint.FindPIPoint(piServer, tagname)
+    pivalues = tag.InterpolatedValues(timeRange, span, "", False)
+    return pd.DataFrame([__value_to_dict(x.Value, x.Timestamp.UtcTime) for x in pivalues])
+
+def __value_to_dict(value, timestamp):
+    local_tz = pytz.timezone('Europe/Amsterdam')
+    return {
+        'Value': value,
+        'Timestamp': datetime.datetime(timestamp.Year,
+                                       timestamp.Month,
+                                       timestamp.Day,
+                                       timestamp.Hour,
+                                       timestamp.Minute,
+                                       timestamp.Second,
+                                       timestamp.Millisecond*1000).replace(tzinfo = pytz.utc).astimezone(local_tz)
+    }
+
+def __disconnect():
+    global piServer
+    global piafServer
+    if piServer:
+        piServer.Disconnect()
+        piServer = None
+    if piafServer:
+        piafServer.Disconnect()
+        piafServer = None
+atexit.register(__disconnect)
