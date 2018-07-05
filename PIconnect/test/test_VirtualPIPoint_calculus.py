0fefca36588b63bb3c49c90ba822a7917c3139ce
Hugo van den Berg <hugo.van.den.berg@brabantwater.nl>
2018-07-05 21:10:01 +0200

    Added imports from `future` and a few imports of the builtin `str` type where necessary

--- PIconnect/test/test_VirtualPIPoint_calculus.py
+++ PIconnect/test/test_VirtualPIPoint_calculus.py
@@ -1,4 +1,11 @@
 """Test VirtualPIPoint calculus."""
+from __future__ import (absolute_import, division,
+                        print_function, unicode_literals)
+from builtins import (bytes, dict, int, list, object, range, str,
+                      ascii, chr, hex, input, next, oct, open,
+                      pow, round, super,
+                      filter, map, zip)
+
 import PIconnect as PI
 from PIconnect.test.fakes import VirtualTestCase
 
