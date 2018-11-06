3403f5131edfa866e61556fe078bcc6317b364d4
Hugo van den Berg <hugo@tbdwebdesign.nl>
2018-11-06 19:14:18 +0100

    Initial try to mock out the SDK if it fails to import

--- PIconnect/AFSDK.py
+++ PIconnect/AFSDK.py
@@ -28,16 +28,23 @@ from builtins import (bytes, dict, int, list, object, range, str,
 import sys
 import os
 
-import clr
+try:
+    import clr
 
-# Get the installation directory from the environment variable or fall back to the Windows
-# default installation path
-PIAF_SDK = os.getenv('PIHOME', 'C:\\Program Files\\PIPC')
-PIAF_SDK += '\\AF\\PublicAssemblies\\4.0\\'
-if not os.path.isdir(PIAF_SDK):
-    raise ImportError('PIAF SDK not found in %s, check installation' % PIAF_SDK)
+    # Get the installation directory from the environment variable or fall back to the Windows
+    # default installation path
+    PIAF_SDK = os.getenv('PIHOME', 'C:\\Program Files\\PIPC')
+    PIAF_SDK += '\\AF\\PublicAssemblies\\4.0\\'
+    if not os.path.isdir(PIAF_SDK):
+        raise ImportError(
+            'PIAF SDK not found in %s, check installation' % PIAF_SDK)
 
-sys.path.append(PIAF_SDK)
-clr.AddReference('OSIsoft.AFSDK')  # pylint: disable=no-member
+    sys.path.append(PIAF_SDK)
+    clr.AddReference('OSIsoft.AFSDK')  # pylint: disable=no-member
 
-from OSIsoft import AF  # pylint: disable=import-error, wrong-import-position
+    from OSIsoft import AF  # pylint: disable=import-error, wrong-import-position
+except ImportError:
+    import warnings
+    warnings.warn("Can't import the PI AF SDK, running in test mode using a fake SDK",
+                  ImportWarning)
+    from PIconnect.test.AFSDKmock import AF
