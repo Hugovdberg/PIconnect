34f7bfcb7ed4cccf547d5c72d2191cef91afa750
Hugo van den Berg <hugo@tbdwebdesign.nl>
2018-11-20 23:19:52 +0100

    Modified requirements for pylint to keep Python 2 compatibility for now; Dropped custom theme

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,6 @@ import PIconnect
 import os
 import sys
 from unittest.mock import MagicMock
-import sphinx_bootstrap_theme
 #sys.path.insert(0, os.path.abspath('..'))
 
 
@@ -47,7 +46,7 @@ sys.path.insert(0, project_root)
 
 # If your documentation needs a minimal Sphinx version, state it here.
 #
-needs_sphinx = '1.8'
+# needs_sphinx = '1.8'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
@@ -108,18 +107,18 @@ todo_include_todos = True
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'bootstrap'
-html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
+html_theme = 'default'
+# html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-html_theme_options = {
-    'bootswatch_theme': 'flatly',
-}
-html_context = {
-    'css_files': ['_static/custom.css'],
-}
+# html_theme_options = {
+#     'bootswatch_theme': 'flatly',
+# }
+# html_context = {
+#     'css_files': ['_static/custom.css'],
+# }
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
