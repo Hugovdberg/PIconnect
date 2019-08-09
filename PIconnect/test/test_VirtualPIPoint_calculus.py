8211808624e42eab807910cffd53f9f3f1a751bc
Hugo van den Berg <hugo.van.den.berg@brabantwater.nl>
2019-08-09 14:44:08 +0200

    Removed unittest2 dependency

--- PIconnect/test/test_VirtualPIPoint_calculus.py
+++ PIconnect/test/test_VirtualPIPoint_calculus.py
@@ -18,74 +18,103 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+
 # pragma pylint: disable=unused-import
-from __future__ import (absolute_import, division,
-                        print_function, unicode_literals)
-from builtins import (bytes, dict, int, list, object, range, str,
-                      ascii, chr, hex, input, next, oct, open,
-                      pow, round, super,
-                      filter, map, zip)
+from __future__ import absolute_import, division, print_function, unicode_literals
+from builtins import (
+    bytes,
+    dict,
+    int,
+    list,
+    object,
+    range,
+    str,
+    ascii,
+    chr,
+    hex,
+    input,
+    next,
+    oct,
+    open,
+    pow,
+    round,
+    super,
+    filter,
+    map,
+    zip,
+)
+
 # pragma pylint: enable=unused-import
 
 import PIconnect as PI
-from PIconnect.test.fakes import VirtualTestCase
+from PIconnect.test.fakes import pi_point  # pylint: disable=unused-import
 
 
-class TestVirtualAddition(VirtualTestCase):
+class TestVirtualAddition:
     """Test VirtualPIPoint addition."""
 
-    def test_add_integer_current_value(self):
+    def test_add_integer_current_value(self, pi_point):
         """Test adding an integer to a PIPoint via the current value."""
-        point2 = self.point + 1
-        self.assertAlmostEqual(point2.current_value, self.values[-1] + 1)
+        point2 = pi_point.point + 1
+        assert round(point2.current_value - (pi_point.values[-1] + 1), ndigits=7) == 0
 
-    def test_add_integer_reverse_current_value(self):
+    def test_add_integer_reverse_current_value(self, pi_point):
         """Test adding a PIPoint to an integer via the current value."""
-        point2 = 1 + self.point
-        self.assertAlmostEqual(point2.current_value, self.values[-1] + 1)
+        point2 = 1 + pi_point.point
+        assert round(point2.current_value - (pi_point.values[-1] + 1), ndigits=7) == 0
 
-    def test_add_pipoints_current_value(self):
+    def test_add_pipoints_current_value(self, pi_point):
         """Test adding two PIPoints via the current value."""
-        total = self.point + self.point
-        self.assertAlmostEqual(total.current_value,
-                               self.values[-1] + self.values[-1])
+        total = pi_point.point + pi_point.point
+        assert (
+            round(
+                total.current_value - (pi_point.values[-1] + pi_point.values[-1]),
+                ndigits=7,
+            )
+            == 0
+        )
 
 
-class TestVirtualMultiplication(VirtualTestCase):
+class TestVirtualMultiplication:
     """Test VirtualPIPoint addition."""
 
-    def test_multiply_integer_current_value(self):
+    def test_multiply_integer_current_value(self, pi_point):
         """Test adding an integer to a PIPoint via the current value."""
-        point2 = self.point * 1
-        self.assertAlmostEqual(point2.current_value, self.values[-1] * 1)
+        point2 = pi_point.point * 1
+        assert round(point2.current_value - (pi_point.values[-1] * 1), ndigits=7) == 0
 
-    def test_multiply_integer_reverse_current_value(self):
+    def test_multiply_integer_reverse_current_value(self, pi_point):
         """Test adding a PIPoint to an integer via the current value."""
-        point2 = 1 * self.point
-        self.assertAlmostEqual(point2.current_value, self.values[-1] * 1)
+        point2 = 1 * pi_point.point
+        assert round(point2.current_value - (pi_point.values[-1] * 1), ndigits=7) == 0
 
-    def test_multiply_pipoints_current_value(self):
+    def test_multiply_pipoints_current_value(self, pi_point):
         """Test adding two PIPoints via the current value."""
-        total = self.point * self.point
-        self.assertAlmostEqual(total.current_value,
-                               self.values[-1] * self.values[-1])
+        total = pi_point.point * pi_point.point
+        assert (
+            round(
+                total.current_value - (pi_point.values[-1] * pi_point.values[-1]),
+                ndigits=7,
+            )
+            == 0
+        )
 
-    def test_multiply_integer_two_current_value(self):
+    def test_multiply_integer_two_current_value(self, pi_point):
         """Test adding an integer to a PIPoint via the current value."""
-        point2 = self.point * 2
-        self.assertAlmostEqual(point2.current_value, self.values[-1] * 2)
+        point2 = pi_point.point * 2
+        assert round(point2.current_value - (pi_point.values[-1] * 2), ndigits=7) == 0
 
-    def test_multiply_integer_two_reverse_current_value(self):
+    def test_multiply_integer_two_reverse_current_value(self, pi_point):
         """Test adding a PIPoint to an integer via the current value."""
-        point2 = 2 * self.point
-        self.assertAlmostEqual(point2.current_value, self.values[-1] * 2)
+        point2 = 2 * pi_point.point
+        assert round(point2.current_value - (pi_point.values[-1] * 2), ndigits=7) == 0
 
-    def test_multiply_float_two_current_value(self):
+    def test_multiply_float_two_current_value(self, pi_point):
         """Test adding an integer to a PIPoint via the current value."""
-        point2 = self.point * 2.0
-        self.assertAlmostEqual(point2.current_value, self.values[-1] * 2.0)
+        point2 = pi_point.point * 2.0
+        assert round(point2.current_value - (pi_point.values[-1] * 2.0), ndigits=7) == 0
 
-    def test_multiply_float_two_reverse_current_value(self):
+    def test_multiply_float_two_reverse_current_value(self, pi_point):
         """Test adding a PIPoint to an integer via the current value."""
-        point2 = 2.0 * self.point
-        self.assertAlmostEqual(point2.current_value, self.values[-1] * 2.0)
+        point2 = 2.0 * pi_point.point
+        assert round(point2.current_value - (pi_point.values[-1] * 2.0), ndigits=7) == 0
