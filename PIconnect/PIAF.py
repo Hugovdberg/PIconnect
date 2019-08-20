636e52c8667bdb1562b41495016d133271c7740a
Hugo van den Berg <hugo.van.den.berg@brabantwater.nl>
2019-08-20 11:30:42 +0200

    adds dictionary of databases at module loading

--- PIconnect/PIAF.py
+++ PIconnect/PIAF.py
@@ -62,16 +62,14 @@ class PIAFDatabase(object):
     """PIAFDatabase
 
     Context manager for connections to the PI Asset Framework database.
-
-    .. todo::
-
-        List databases at class loading and remove note in
-        :ref:`connect_piaf_database`.
     """
 
-    version = "0.1.0"
+    version = "0.2.0"
 
-    servers = {x.Name: {"server": x, "databases": {}} for x in AF.PISystems()}
+    servers = {
+        s.Name: {"server": s, "databases": {d.Name: d for d in s.Databases}}
+        for s in AF.PISystems()
+    }
     default_server = servers[AF.PISystems().DefaultPISystem.Name]
 
     def __init__(self, server=None, database=None):
