949ac8831f847a48dc05e9ef9473d3df5ef564c9
Hugo Lapré <hugo.lapre@brabantwater.nl>
2022-12-14 14:21:42 +0100

    Made classproperty generic and typed

--- PIconnect/_utils.py
+++ PIconnect/_utils.py
@@ -1,7 +1,15 @@
-class classproperty(object):
-    # Copyright Denis Ryzhkov, https://stackoverflow.com/a/13624858/4398595
-    def __init__(self, fget):
-        self.fget = fget
+from typing import Callable, Generic, Type, TypeVar
 
-    def __get__(self, owner_self, owner_cls):
+_T = TypeVar("_T")
+_V = TypeVar("_V")
+
+
+class classproperty(Generic[_T, _V]):
+    # Copyright Denis Ryzhkov, https://stackoverflow.com/a/13624858/4398595
+
+    def __init__(self, fget: Callable[[Type[_T]], _V]) -> None:
+        self.fget = fget
+        self.__doc__ = fget.__doc__
+
+    def __get__(self, instance: _T, owner_cls: Type[_T]) -> _V:
         return self.fget(owner_cls)
