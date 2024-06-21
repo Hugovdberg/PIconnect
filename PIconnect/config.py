82ff33e3c73399246a92b3b58ad771e25698af37
Hugo Lapré <hugo.lapre@brabantwater.nl>
2024-06-21 16:23:06 +0200

    feat: remove pytz dependency

--- PIconnect/config.py
+++ PIconnect/config.py
@@ -7,7 +7,10 @@ class PIConfigContainer:
     This should not be instantiated directly, but accessed through the `PIConfig` object.
     """
 
-    _default_timezone: str = "UTC"
+    _default_timezone: str = ""
+
+    def __init__(self) -> None:
+        self.DEFAULT_TIMEZONE = "UTC"
 
     @property
     def DEFAULT_TIMEZONE(self) -> str:
@@ -16,9 +19,9 @@ class PIConfigContainer:
 
     @DEFAULT_TIMEZONE.setter
     def DEFAULT_TIMEZONE(self, value: str) -> None:
-        import pytz
+        import zoneinfo
 
-        if value not in pytz.all_timezones:
+        if value not in zoneinfo.available_timezones():
             raise ValueError("{v!r} not found in pytz.all_timezones".format(v=value))
         self._default_timezone = value
 
