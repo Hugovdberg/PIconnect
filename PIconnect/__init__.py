2f3a75a1666d17401ddc6650b74edb77ac9767a1
Hugo Lapré <hugo.lapre@brabantwater.nl>
2024-06-25 13:42:46 +0200

    feat: implement explicit loader for SDK

--- PIconnect/__init__.py
+++ PIconnect/__init__.py
@@ -1,12 +1,9 @@
 """PIconnect - Connector to the OSISoft PI and PI-AF databases."""
 
-# pragma pylint: disable=unused-import
-from PIconnect.AFSDK import AF, AF_SDK_VERSION
+from PIconnect.AFSDK import AF, AF_SDK_VERSION, get_PI_connector
 from PIconnect.config import PIConfig
 from PIconnect.PI import PIServer
 from PIconnect.PIAF import PIAFDatabase
-
-# pragma pylint: enable=unused-import
 
 __version__ = "0.12.0"
 __sdk_version = tuple(int(x) for x in AF.PISystems().Version.split("."))
@@ -17,5 +14,6 @@ __all__ = [
     "PIAFDatabase",
     "PIConfig",
     "PIServer",
+    "get_PI_connector",
     "__sdk_version",
 ]
