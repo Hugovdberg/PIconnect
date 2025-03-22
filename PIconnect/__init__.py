64e29a3594d54204bf8e36b5c3f3d3215de23eb9
Hugo Lapré <hugo.lapre@brabantwater.nl>
2025-03-22 13:32:41 +0100

    fix: fixed typo in package list

--- PIconnect/__init__.py
+++ PIconnect/__init__.py
@@ -8,7 +8,7 @@ from PIconnect.PIAF import PIAFDatabase
 
 # pragma pylint: enable=unused-import
 
-__version__ = "0.11.3"
+__version__ = "0.11.4"
 __sdk_version = tuple(int(x) for x in AF.PISystems().Version.split("."))
 
 __all__ = [
